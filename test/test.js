0e8156c86cf17e94d52cd881c948e1a1e476ea5a
Douglas Gardner <douglas@chippy.ch>
2016-09-20 23:12:12 +0100

    Add basic tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 /* eslint-env mocha */
+/**
+ * These are mostly just smoke tests
+ */
 var app = require('..')
 
 require('should')
@@ -51,6 +54,58 @@ describe('templating language compilation', function () {
         if (err) throw err
         // check for string that is only in an unbuffered comment
         res.text.should.not.containEql('squeamish ossifrage')
+        done()
+      })
+  })
+})
+
+describe('applicant listings', function () {
+  it('displays something with a real applicant', function (done) {
+    request(app)
+      .get('/applicant/0')
+      .expect(200, done)
+  })
+  it('404s when no applicant exists', function (done) {
+    request(app)
+      .get('/applicant/-1')
+      .expect(404, done)
+  })
+  it('displays the applicant name somewhere on the listing', function (done) {
+    request(app)
+      .get('/applicant/0')
+      .end(function (err, res) {
+        if (err) throw err
+        res.text.should.containEql('Gaston')
+        res.text.should.containEql('Leroux')
+        done()
+      })
+  })
+  it('displays the applicant experience somewhere on the listing', function (done) {
+    request(app)
+      .get('/applicant/0')
+      .end(function (err, res) {
+        if (err) throw err
+        res.text.should.containEql('Christine Daae')
+        res.text.should.containEql('Opera')
+        res.text.should.containEql('story')
+        done()
+      })
+  })
+})
+describe('applicants list', function () {
+  it('displays something', function (done) {
+    request(app)
+      .get('/')
+      .expect(200, done)
+  })
+  it('displays some applicants', function (done) {
+    request(app)
+      .get('/')
+      .end(function (err, res) {
+        if (err) throw err
+        res.text.should.containEql('Buchan')
+        res.text.should.containEql('Dumas')
+        res.text.should.containEql('Leroux')
         done()
       })
   })
